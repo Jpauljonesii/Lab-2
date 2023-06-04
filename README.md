f9b17d56744d930d06f640fad116613749aeedb1
John P. Jones II <jpauljonesii@gmail.com>
2023-06-03 19:59:54 -0500

    updated javascript file and readme file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Lab-2
+
+## About Me
+
+I built a website to provide with a brief bio for myself and as a means to get to know any visitors to my page. 
+
+Author: John P. Jones II
+
+Links and Resources
+
+-[About me site](https://jpauljonesii.github.io/Lab-2/index.html)
+
+Lighthouse Accessibility Score
+
+Reflections and Comments
+
+-This assignment took me some time to complete.  I am still learning and getting a grasp on javascript and trying to incorporate the new things we are learning.  
--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,19 @@
 'use script';
 
 let username = prompt('What is your name?');
-// console.log(`The user's name is ${username} hello there ${username}`);
+// console.log('username');
 
 let hometown = prompt('What is your hometown?');
-// console.log('The user is from ${hometown}');
+// console.log('hometown');
 
 let food = prompt('Is your favorite food pizza?');
 switch (food.toLowerCase()){
 case 'yes':
-  // console.log('great choice');
+  // console.log('food');
   alert ('great choice');
   break;
 case 'no':
-  // console.log('that sucks');
+  // console.log('food');
   alert ('that sucks');
 
 }
@@ -21,44 +21,44 @@ case 'no':
 let travel = prompt('Do you like to travel?');
 switch (travel.toLowerCase()){
 case 'yes':
-  // console.log('traveling is great');
+  // console.log('travel');
   alert ('traveling is great');
   break;
 case 'no':
-  // console.log('you are missing out');
+  // console.log('travel');
   alert ('you are missing out');
 }
 
 let sports = prompt('Do you like watching sports?');
 switch (sports.toLowerCase()){
 case 'yes':
-  // console.log('sports are exciting');
+  // console.log('sports');
   alert ('sports are exciting');
   break;
 case 'no':
-  // console.log('you are missing out');
+  // console.log('sports');
   alert ('you are missing out');
 }
 
 let kids = prompt('Do you have any kids');
 switch (kids.toLowerCase()){
 case 'yes':
-  // console.log('kids are a blessing');
+  // console.log('kids');
   alert ('kids are a blessing');
   break;
 case 'no':
-  // console.log('you have it made lol');
+  // console.log('kids');
   alert ('you have it made lol');
 }
 
 let read = prompt ('Do you like to read');
 switch (read.toLowerCase()){
 case 'yes':
-  // console.log('reading is good for you');
+  // console.log('read');
   alert ('reading helps you grow');
   break;
 case 'no':
-  // console.log('you cant grow if you dont read');
+  // console.log('read');
   alert ('you cant grow if you dont read');
 
 }
